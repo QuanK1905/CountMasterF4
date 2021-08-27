7fdd55c229f339f1dfef5e4d7aeec8b4f548a96d
QuanK1905 <55221917+QuanK1905@users.noreply.github.com>
2021-08-27 08:51:01 +0700

    .

--- Assets/JetSystems/JetUI/Scripts/Scriptable Objects/ColorPalette.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Color Palette", menuName = "Scriptable Objects/Color Palette", order = 1)]
-public class ColorPalette : ScriptableObject
-{
-    public List<Color> colors;
-}
